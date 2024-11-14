66660bece1c08dc1c1500527099d6667b38a239e
Arafath-21 <arafathyas21@gmail.com>
2024-11-14 21:47:17 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+let lname = "john";
+console.log(lname.length);
+let num = 10;
+num = 11;
+let isValid = false;
+console.log(isValid);
---
+++ datatypes.ts
@@ -0,0 +1,10 @@
+let lname: string = "john";
+
+console.log(lname.length);
+
+let num: Number = 10;
+
+num = 11;
+
+let isValid: boolean = false;
+console.log(isValid);
